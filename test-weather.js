63cfd480e5251730fb7e3b4eb883a8038ea40f5a
Joseph Carey <git@joecarey.com>
2026-02-19 15:13:35 -0600

    Remove test scripts

--- test-weather.js
+++
@@ -1,5 +0,0 @@
-import { WeatherTool } from './src/tools/weather.js';
-
-const tool = new WeatherTool();
-const result = await tool.execute({ location: 'Austin Texas' });
-console.log('Result:', result);
